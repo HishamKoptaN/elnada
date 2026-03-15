3502ec7f186ad70d2fa743847297eebc4e94c726
Hisham Mohamed <heshamkoptan@gmail.com>
2026-03-15 06:57:53 +0200

    2.0.0+1

--- lib/features/customers/present/views/customers_view.dart
+++ lib/features/customers/present/views/customers_view.dart
@@ -57,7 +57,7 @@ class _CustomersViewState extends State<CustomersView> {
               spacing: 5.h,
               children: [
                 Text(
-                  '4.0.0+1',
+                  '2.0.0+1',
                   style: TextStyle(fontSize: 10.sp, color: Colors.black),
                 ),
                 DateHeaderWidget(state: state),
